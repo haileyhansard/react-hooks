692a71d32c1d2d1c5c996edc2889db0c36156c3a
Hailey Hansard <haileyhansard@gmail.com>
2021-03-12 11:43:17 -0800

    Exercise 02 lazy state initialization, effect dependency array

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,11 +6,14 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') || initialName,
+  )
+  //Above is the lazy state initialization Extra Credit. This uses an arrow function to return the localStorage upon the first time the component is rendered. useState allows you to pass a function instead of the actual value, and then it will only call that function to get the state value when the component is rendered the first time.
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
-  // 💰 
+  // 💰
 
   //initialName is set to an empty string initially
   //useState: get name item out of localStorage, or if there is none, then get initialName
@@ -18,7 +21,7 @@ function Greeting({initialName = ''}) {
   //useEffect synchronizes the state of the world (localStorage) with the state of the application (the name value)
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  })
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
